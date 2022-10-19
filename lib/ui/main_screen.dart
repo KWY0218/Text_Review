1b77bc3aaf39d3c8f0ebf508f8abd9381ed7549c
KWY0218 <wodrk1001@gmail.com>
2022-10-19 22:28:03 +0900

    [ui] ui 기초 구현

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -14,8 +14,9 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print("data ${getPosts()}");
-    return Scaffold();
+    return Scaffold(
+      body: PostList(getPosts()),
+    );
   }
 
   Future<List<Post>> getPosts() async {
@@ -25,5 +26,57 @@ class _MainScreenState extends State<MainScreen> {
     final postsSnapShot = await postsCollection.get();
     final posts = postsSnapShot.docs.map((doc) => doc.data()).toList();
     return posts;
+  }
+}
+
+class PostList extends StatelessWidget {
+  PostList(this._posts);
+
+  final Future<List<Post>> _posts;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Post>>(
+        future: _posts,
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return PostItem(snapshot.data![index]);
+                });
+          } else {
+            return Text("loading");
+          }
+        });
+  }
+}
+
+class PostItem extends StatelessWidget {
+  PostItem(this._post);
+
+  final Post _post;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(_post.content!),
+      trailing: LikeIcon(_post.isLike!),
+    );
+  }
+}
+
+class LikeIcon extends StatelessWidget {
+  LikeIcon(this._isLike);
+
+  final bool _isLike;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLike) {
+      return const Icon(Icons.favorite);
+    } else {
+      return const Icon(Icons.favorite_border);
+    }
   }
 }
